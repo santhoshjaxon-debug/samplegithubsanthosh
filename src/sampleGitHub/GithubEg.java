cbc6577da941f07f2bf4ad15c82794141baccada
santhoshjaxon-debug <santhosh.jaxon@gmail.com>
2025-12-31 12:56:04 +0530

    ProjectJava

---
+++ src/sampleGitHub/GithubEg.java
@@ -0,0 +1,13 @@
+package sampleGitHub;
+
+public class GithubEg {
+	public void employename() {
+		System.out.println("Employee name is Donglee");
+	}
+	
+	public static void main(String[] args) {
+		GithubEg g = new GithubEg();
+		g.employename();
+	}
+	
+}
